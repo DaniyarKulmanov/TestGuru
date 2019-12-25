238cfb86fb367801e7de103370df64940b2badfd
DaniyarKulmanov <danrubydeveloper@gmail.com>
2019-12-25 09:06:17 +0600

    User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_25_030053) do
+ActiveRecord::Schema.define(version: 2019_12_25_030416) do
 
   create_table "answers", force: :cascade do |t|
     t.string "correct", default: "Yes"
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 2019_12_25_030053) do
     t.string "title", null: false
     t.integer "level", default: 0
     t.integer "c_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

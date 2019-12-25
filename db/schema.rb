c1263c91ad88b2cab342b465be3cd9adca4ec973
DaniyarKulmanov <danrubydeveloper@gmail.com>
2019-12-25 09:03:29 +0600

    Answer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_25_025740) do
+ActiveRecord::Schema.define(version: 2019_12_25_030053) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string "correct", default: "Yes"
+    t.integer "question_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "title", null: false

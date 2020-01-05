c3926b6c3db4d52a097659f325793d70f0ed9da4
DaniyarKulmanov <danrubydeveloper@gmail.com>
2020-01-05 13:48:40 +0600

    class method test_by_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,11 @@
 class Test < ApplicationRecord
+  JOIN_CATEGORY = 'INNER JOIN categories ON categories.id = tests.category_id'.freeze
+
+  def self.test_by_category (category)
+    Test
+      .select(:title)
+      .joins(JOIN_CATEGORY)
+      .order(title: :desc)
+      .where('categories.title' => category)
+  end
 end

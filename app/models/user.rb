0b1bf55c9db7fddcb04eb30a5e7f824c8498454b
DaniyarKulmanov <danrubydeveloper@gmail.com>
2020-01-09 21:39:50 +0600

    part 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   JOIN_CERTIFICATION = 'INNER JOIN certifications ON certifications.test_id = tests.id'.freeze
 
+  has_many :certifications
+  has_many :tests, through: :certifications
+
   def test_by_level (level)
     Test
       .joins(JOIN_CERTIFICATION)
--- db/migrate/20200106053305_create_tests_users_join_table.rb
+++ db/migrate/20200106053305_create_tests_users_join_table.rb
@@ -1,8 +1,7 @@
 class CreateTestsUsersJoinTable < ActiveRecord::Migration[6.0]
   def change
     create_join_table :tests, :users, table_name: :certifications do |t|
-      t.index :test_id
-      t.index :user_id
+      t.index [:test_id, :user_id]
 
       t.timestamps
     end

46a3a0062d29f91b4a54781989b5ad286ea3afdb
DaniyarKulmanov <danrubydeveloper@gmail.com>
2020-02-03 23:50:47 +0600

    7.3 and other

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,24 +1,31 @@
 class QuestionsController < ApplicationController
-  before_action :search_test, only: :show
+  before_action :search_test, only: [:index, :show, :destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
-    render  plain: 'hello'
+    render  plain: @test.questions.each{ |question| question }
   end
 
   def show
-    
+    body = @test.questions.find(params[:id]).body
+    render plain: body
   end
 
   def new
-    
+    # @questoin = Question.new
   end
 
   def create
-    
+    question = Question.create(question_params)
+    render plain: question.inspect
   end
 
   def destroy
-    
+    @question = @test.questions.find(params[:id])
+    @question.destroy
+
+    redirect_to questions_path
   end
 
   private
@@ -28,6 +35,10 @@ class QuestionsController < ApplicationController
   end
 
   def search_test
-    
+    @test = Test.find(params[:test_id])
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'No question found'
   end
 end

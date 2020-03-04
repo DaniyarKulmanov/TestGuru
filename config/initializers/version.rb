71117ea9080a32a6af0794abefa830954ecba135
DaniyarKulmanov <danrubydeveloper@gmail.com>
2020-03-04 07:59:31 +0600

    add body to answers

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,6 +1,7 @@
 class SiteVersion
   OLD_VERSION = '0.8.0'.freeze
   #TODO form_with model: [@test, @question] should work for new/edit
-  CURRENT_VERSION = '0.9.0'.freeze
+  CURRENT_VERSION = '0.9.1'.freeze
   #TODO rails db:setup || db:drop cause error no such table: main.current_questions
+  #TODO answers - view & controller
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_022157) do
+ActiveRecord::Schema.define(version: 2020_03_04_014128) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false
     t.integer "question_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "body"
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
